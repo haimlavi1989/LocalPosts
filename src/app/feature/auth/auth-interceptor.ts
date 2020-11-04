adbcdc24bacb0bb7467a7064fcf6571b5ab9acb2
haim lavi <haimlavi1989@gmail.com>
2020-11-04 12:13:26 +0200

    add authentication logic

---
+++ src/app/feature/auth/auth-interceptor.ts
@@ -0,0 +1,28 @@
+import {
+    HttpInterceptor,
+    HttpRequest,
+    HttpHandler
+  } from "@angular/common/http";
+  import { Injectable } from "@angular/core";
+  
+  @Injectable()
+  export class AuthInterceptor implements HttpInterceptor {
+
+    constructor() {}
+  
+    intercept(req: HttpRequest<any>, next: HttpHandler) {
+
+      const authToken = localStorage.getItem("token");
+      
+      if (authToken) {
+        const cloned  = req.clone({
+          headers: req.headers.set("Authorization", "Bearer " + authToken)
+        });
+
+        return next.handle(cloned);
+      } 
+      else {
+        return next.handle(req);
+      }
+    }
+  }
---
+++ src/app/feature/auth/auth.service.ts
@@ -0,0 +1,121 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from "@angular/router";
+import { Subject } from "rxjs";
+
+import { AuthData } from "./auth-data.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private url: string = "https://localpostsbackend.herokuapp.com";
+
+  private isAuthenticated = false;
+  private token: string;
+  private tokenTimer: any;
+  private authStatusListener = new Subject<boolean>();
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  getIsAuth() {
+    return this.isAuthenticated;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
+  }
+
+  createUser(email: string, password: string) {
+    const authData: AuthData = { email: email, password: password };
+    this.http
+      .post(this.url+"/api/v1/users/signup", authData)
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
+
+  login(email: string, password: string) {
+    const authData: AuthData = { email: email, password: password };
+    this.http
+      .post<{ status: string, token: string; expiresIn: string;}>(
+        this.url+"/api/v1/users/login",
+        authData
+      )
+      .subscribe(response => {
+        if (response.status === 'success') {
+          const token = response.token;
+          this.token = token;
+          const expiresInDuration = this.convertHourstoSeconds(response.expiresIn.replace('h',''));;
+          this.setAuthTimer(expiresInDuration);
+          this.isAuthenticated = true;
+          this.authStatusListener.next(true);
+          const now = new Date();
+          const expirationDate = new Date(now.getTime() + expiresInDuration * 1000);
+          this.saveAuthData(token, expirationDate);
+          this.router.navigate(["/posts"]);
+        }
+      });
+  }
+
+  autoAuthUser() {
+    const authInformation = this.getAuthData();
+    if (!authInformation) {
+      return;
+    }
+    const now = new Date();
+    const expiresIn = authInformation.expirationDate.getTime() - now.getTime();
+    if (expiresIn > 0) {
+      this.token = authInformation.token;
+      this.isAuthenticated = true;
+      this.setAuthTimer(expiresIn / 1000);
+      this.authStatusListener.next(true);
+    }
+  }
+
+  logout() {
+    this.token = null;
+    this.isAuthenticated = false;
+    this.authStatusListener.next(false);
+    clearTimeout(this.tokenTimer);
+    this.clearAuthData();
+    this.router.navigate(["/"]);
+  }
+
+  private setAuthTimer(duration: number) {
+    console.log("Setting timer: " + duration);
+    this.tokenTimer = setTimeout(() => {
+      this.logout();
+    }, duration * 1000);
+  }
+
+  private saveAuthData(token: string, expirationDate: Date) {
+    localStorage.setItem("token", token);
+    localStorage.setItem("expiration", expirationDate.toISOString());
+  }
+
+  private clearAuthData() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("expiration");
+  }
+
+  private getAuthData() {
+    const token = localStorage.getItem("token");
+    const expirationDate = localStorage.getItem("expiration");
+    if (!token || !expirationDate) {
+      return;
+    }
+    return {
+      token: token,
+      expirationDate: new Date(expirationDate)
+    }
+  }
+
+  private convertHourstoSeconds(hours) 
+  {
+    return Math.floor(hours * 60 * 60);
+  }
+
+
+}
