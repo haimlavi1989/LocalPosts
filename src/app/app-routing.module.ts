e7736a0da38e7aac7de8c964b9f5cb59c0a8ea91
haim lavi <haimlavi1989@gmail.com>
2020-12-28 16:59:42 +0200

    fix posts categorey select

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,20 +5,22 @@ import { RegisterComponent } from './feature/register/register.component';
 import { CategoriesComponent } from './feature/posts/categories/categories.component';
 import { PostsonmapComponent } from './feature/posts/postsonmap/postsonmap.component';
 import { PostsComponent } from './feature/posts/posts.component';
+import { AuthGuard } from "./feature/auth/auth.guard";
 
  const routes: Routes = [
   { path : 'login', component : LoginComponent },
   { path : 'register', component : RegisterComponent },
   { path : 'categories', component : CategoriesComponent },
-  { path : 'postsonmap', component : PostsonmapComponent },
+  { path : 'postsonmap', component : PostsonmapComponent, canActivate: [AuthGuard]},
   { path : 'posts', component : PostsComponent },
-  { path : 'posts/:id', component : PostsComponent },
+  { path : 'posts/:subjectName', component : PostsComponent },
   { path: '', redirectTo: 'register', pathMatch: 'full' },
   ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { 
 
