70a8289d84a464111bd2118c6b127b313d190bf7
haim lavi <haimlavi1989@gmail.com>
2021-03-25 12:56:32 +0200

    add posts resolver

--- src/app/feature/posts/posts-resolver.service.ts
+++ src/app/feature/posts/posts-resolver.service.ts
@@ -22,6 +22,7 @@ export class PostsResolverService implements Resolve<any> {
   ) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    this.postService.initPosts();
   }
 
 }
--- src/app/feature/posts/posts-routing.module.ts
+++ src/app/feature/posts/posts-routing.module.ts
@@ -18,10 +18,12 @@ const routes: Routes = [
     { path : 'posts/:subjectName',
       component : PostsComponent,
       canActivate: [AuthGuard],
+      resolve: [PostsResolverService]
     },
     { path : 'postdetail/:id',
       component : DetailComponent,
-      canActivate: [AuthGuard]
+      canActivate: [AuthGuard],
+      resolve: [PostsResolverService]
     },
 ];
 
