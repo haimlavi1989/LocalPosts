97627b433cd0d4ee8afb8af1cc33d30465a05fee
haim lavi <haimlavi1989@gmail.com>
2021-04-19 21:52:57 +0300

    Ad search componenet outline

--- src/app/feature/search/search.component.ts
+++ src/app/feature/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  searchPostFormGroup: FormGroup;
+  search: FormControl;
+  isLoadingSearch: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  createFormControls() {
+    this.search = new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(50)]);
+  }
+
+  initForm() {
+    this.createFormControls();
+
+    this.searchPostFormGroup = new FormGroup({
+      search: this.search,
+    });
+  }
+
+  onSubmit(form: FormGroup) {
   }
 
 }
